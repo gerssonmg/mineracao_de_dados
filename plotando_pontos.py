aeac80dc0ec1ada3fda1326b7458145d830dbf24
Gerson <gersoncafilho@gmail.com>
2018-08-21 23:06:13 -0300

    0.0.3
    Adicionando Comentarios.

--- plotando_pontos.py
+++ plotando_pontos.py
@@ -9,33 +9,29 @@ from numpy import matrix
 import matplotlib.pyplot as plt
 from random import randint
 
- 
+"""criando X1 e X2 com valores aleatorios"""
 X1 = []
 X2 = []
 for it_iteretor in range(100):
     X1.append(randint(0,10))
     X2.append(randint(0,10))
-   
-X_line_vertical = [0,0]#,200,300,400,500,600]
-Y_line_vertical = [0,10]#,200,300,400,500,600]
-
+"""Plotando o grafico de X1 , X2  / Argumento 'ro' para plotar apenas o pontos """
 plt.plot(X1,X2,'ro')
 
-def calc(a , x , b):
-    return a * x + b
+
+"""Plotando as barras azuis verticais"""   
+Y_line_vertical = [0,10]
 for cont in range(2):
     plt.plot([cont * 5,cont * 5],Y_line_vertical,color='b') 
 
-
-X_line_horizontal = [0,500]#,200,300,400,500,600]
-Y_line_horizontal = [0,0]#,200,300,400,500,600]
-
+"""Plotando as barras azuis horizontais"""
+X_line_horizontal = [0,500]
 for cont in range(2):
     plt.plot(X_line_horizontal,[cont * 5,cont * 5],color='b') 
 
 
+"""Calculando a soma dos valores que estão no mesmo quadrante"""
 Q1 = 0
-
 cont1 = 0
 for n in range(100):
     if X1[n] >= 5  and  X1[n] <= 10 and X2[n]>=5 and X2[n]<=10:
